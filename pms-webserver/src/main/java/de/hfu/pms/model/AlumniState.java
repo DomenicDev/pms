99c4d8025ef6d56f63fc7fe0964d69dd04c84230
Domenic Cassisi <domenic.cassisi@web.de>
2019-04-21 23:19:02 +0200

    Added Getter and Setter methods for Alumni State attributes

--- pms-webserver/src/main/java/de/hfu/pms/model/AlumniState.java
+++ pms-webserver/src/main/java/de/hfu/pms/model/AlumniState.java
@@ -18,4 +18,36 @@ public class AlumniState {
     @Column
     private boolean agreementEvaluation;
 
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public String getEmployer() {
+        return employer;
+    }
+
+    public void setEmployer(String employer) {
+        this.employer = employer;
+    }
+
+    public boolean isAgreementNews() {
+        return agreementNews;
+    }
+
+    public void setAgreementNews(boolean agreementNews) {
+        this.agreementNews = agreementNews;
+    }
+
+    public boolean isAgreementEvaluation() {
+        return agreementEvaluation;
+    }
+
+    public void setAgreementEvaluation(boolean agreementEvaluation) {
+        this.agreementEvaluation = agreementEvaluation;
+    }
 }
