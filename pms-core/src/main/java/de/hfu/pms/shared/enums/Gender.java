dfc53a08a286238fa89734781f7dd679921cbe70
Domenic Cassisi <domenic.cassisi@web.de>
2019-04-21 21:32:03 +0200

    Added JavaDoc and renamed "other"-type to "Miscellaneous".

--- pms-core/src/main/java/de/hfu/pms/shared/enums/Gender.java
+++ pms-core/src/main/java/de/hfu/pms/shared/enums/Gender.java
@@ -5,10 +5,19 @@ package de.hfu.pms.shared.enums;
  */
 public enum Gender {
 
+    /**
+     * Gender type for male persons
+     */
     Male,
 
+    /**
+     * Gender type for female persons
+     */
     Female,
 
-    other;
+    /**
+     * For other gender types than male and female
+     */
+    Miscellaneous;
 
 }
