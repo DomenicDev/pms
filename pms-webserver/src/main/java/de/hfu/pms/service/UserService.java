dab426b121d0a034d6c429557e6f8f947d715e83
david <david.geigle@hs-furtwangen.de>
2019-04-24 13:03:42 +0200

    Added Implementation of create User.

--- pms-webserver/src/main/java/de/hfu/pms/service/UserService.java
+++ pms-webserver/src/main/java/de/hfu/pms/service/UserService.java
@@ -1,14 +1,15 @@
 package de.hfu.pms.service;
 
 import de.hfu.pms.dao.UserDao;
+import de.hfu.pms.shared.dto.UserDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface UserService {
 
-    void createUser();
-    void deleteUser();
+    void createUser(UserDto userDto);
+    void deleteUser(UserDto userDto);
 
 
 }
--- pms-webserver/src/main/java/de/hfu/pms/service/impl/UserServiceImpl.java
+++ pms-webserver/src/main/java/de/hfu/pms/service/impl/UserServiceImpl.java
@@ -1,20 +1,25 @@
 package de.hfu.pms.service.impl;
 
 import de.hfu.pms.dao.UserDao;
+import de.hfu.pms.model.User;
 import de.hfu.pms.service.UserService;
 import de.hfu.pms.shared.dto.UserDto;
 
+import java.util.List;
+
 public class UserServiceImpl implements UserService {
 
+    //@
     private UserDao userDao;
 
     @Override
     public void createUser(UserDto userDto) {
-        userDao.
+        User newUser = new User(userDto.getUsername(),userDto.getPassword());
+        userDao.save(newUser);
+        userDao.flush();
     }
 
     @Override
     public void deleteUser(UserDto userDto) {
-        userDao.delete(userDao);
     }
 }
