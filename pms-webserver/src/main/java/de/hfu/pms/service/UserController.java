e2ec1833246713b5d7960892166cf3c4f310ed07
David Geigle <33023777+David-Geigle@users.noreply.github.com>
2019-04-26 11:28:58 +0200

    Added ipmlementation of deleteUser and updateRole. Added mapping for delete and updateRole.

--- pms-webserver/src/main/java/de/hfu/pms/service/UserController.java
+++ pms-webserver/src/main/java/de/hfu/pms/service/UserController.java
@@ -1,6 +1,8 @@
 package de.hfu.pms.service;
 
 
+import de.hfu.pms.model.UserRole;
+import de.hfu.pms.service.impl.UserServiceImpl;
 import de.hfu.pms.shared.dto.UserDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -9,11 +11,26 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/user")
 public class UserController {
 
+    private UserService service = new UserServiceImpl();
+
     @PostMapping("/create")
     @ResponseStatus(HttpStatus.CREATED)
     public void newUser (@RequestBody UserDto user){
-
+        service.createUser(user);
     }
+
+    @PostMapping("/delete")
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteUser (@RequestBody String username){
+        service.deleteUser(username);
+    }
+
+    @PostMapping("/updateRole")
+    @ResponseStatus(HttpStatus.OK)
+    public void updateUserRole (@RequestBody String username, UserRole role){
+        service.updateUserRole(username,role);
+    }
+
 
 
 }
--- pms-webserver/src/main/java/de/hfu/pms/service/impl/UserServiceImpl.java
+++ pms-webserver/src/main/java/de/hfu/pms/service/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package de.hfu.pms.service.impl;
 
 import de.hfu.pms.dao.UserDao;
 import de.hfu.pms.model.User;
+import de.hfu.pms.model.UserRole;
 import de.hfu.pms.service.UserService;
 import de.hfu.pms.shared.dto.UserDto;
 
@@ -20,6 +21,21 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public void deleteUser(UserDto userDto) {
+    public void deleteUser(String username) {
+        for(User user : userDao.findAll()){
+            if(user.getUsername().equals(username)){
+                userDao.delete(user);
+            }
+        }
+
+    }
+
+    @Override
+    public void updateUserRole(String username, UserRole role) {
+        for(User user :userDao.findAll()){
+            if(user.getUsername().equals(username)){
+                user.setRole(role);
+            }
+        }
     }
 }
