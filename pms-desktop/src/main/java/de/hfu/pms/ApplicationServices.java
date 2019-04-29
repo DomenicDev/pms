b32dd15b99d8c2cf591fb2a01ca56ec8f6181ad8
Domenic Cassisi <domenic.cassisi@web.de>
2019-04-29 13:27:48 +0200

    Added Client-server interface for communication

---
+++ pms-desktop/src/main/java/de/hfu/pms/ApplicationServices.java
@@ -0,0 +1,11 @@
+package de.hfu.pms;
+
+import de.hfu.pms.shared.dto.DoctoralStudentDTO;
+
+public interface ApplicationServices {
+
+    void addDoctoralStudent(DoctoralStudentDTO student);
+
+    // Todo add all other needed methods
+
+}
