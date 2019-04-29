15039f28da4e562f53fcc192d387145103db6be2
david <david.geigle@hs-furtwangen.de>
2019-04-29 13:04:17 +0200

    Removed the Id of the user, now is the username the primary key. Updateted all demendent clases.

--- pms-webserver/src/main/java/de/hfu/pms/dao/UserDao.java
+++ pms-webserver/src/main/java/de/hfu/pms/dao/UserDao.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserDao extends JpaRepository<User, Long> {
+public interface UserDao extends JpaRepository<User, String> {
 }
