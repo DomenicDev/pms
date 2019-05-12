ea67fa18fb0fb58292b8789f59f4204203b0fb43
Domenic Cassisi <domenic.cassisi@web.de>
2019-05-12 02:51:36 +0200

    Added sample Doctoral Student for testing (to be extended...)

--- pms-webserver/src/main/java/de/hfu/pms/DatabaseInit.java
+++ pms-webserver/src/main/java/de/hfu/pms/DatabaseInit.java
@@ -1,8 +1,12 @@
 package de.hfu.pms;
 
+import de.hfu.pms.dao.DoctoralStudentDao;
 import de.hfu.pms.dao.UserDao;
+import de.hfu.pms.model.DoctoralStudent;
+import de.hfu.pms.model.PersonalData;
 import de.hfu.pms.model.User;
 import de.hfu.pms.model.UserRole;
+import de.hfu.pms.shared.enums.Gender;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -17,10 +21,13 @@ public class DatabaseInit implements CommandLineRunner {
     private UserDao userDao;
     private PasswordEncoder passwordEncoder;
 
+    private final DoctoralStudentDao doctoralStudentDao;
+
     @Autowired
-    public DatabaseInit(UserDao userDao, PasswordEncoder passwordEncoder) {
+    public DatabaseInit(UserDao userDao, PasswordEncoder passwordEncoder, DoctoralStudentDao doctoralStudentDao) {
         this.userDao = userDao;
         this.passwordEncoder = passwordEncoder;
+        this.doctoralStudentDao = doctoralStudentDao;
     }
 
     @Override
@@ -29,5 +36,19 @@ public class DatabaseInit implements CommandLineRunner {
         User testAdmin = new User("admin", passwordEncoder.encode("1234"),"test@example.com","Bob","Tester");
         testAdmin.setRole(UserRole.ADMIN);
         userDao.save(testAdmin);
+
+        // add simple doctoral student
+        DoctoralStudent student = new DoctoralStudent();
+
+        PersonalData personalData = new PersonalData();
+        personalData.setForename("Max");
+        personalData.setLastName("Mustermann");
+        personalData.setGender(Gender.Male);
+        personalData.setNationality("German");
+
+        student.setPersonalData(personalData);
+
+        doctoralStudentDao.save(student);
+
     }
 }
