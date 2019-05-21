90d1b442ad8fd10870fad4b5428563a19540c952
Benedikt Neubauer <benni.neubauer@gmx.net>
2019-05-21 19:53:03 +0200

    application logic for university added

--- pms-desktop/src/main/java/de/hfu/pms/controller/UniversityAddController.java
+++ pms-desktop/src/main/java/de/hfu/pms/controller/UniversityAddController.java
@@ -1,5 +1,10 @@
 package de.hfu.pms.controller;
 
+import com.google.common.eventbus.EventBus;
+import de.hfu.pms.eventbus.EventBusSystem;
+import de.hfu.pms.events.SuccessfullyAddedUniversityEvent;
+import de.hfu.pms.shared.dto.UniversityDTO;
+import de.hfu.pms.utils.FormValidator;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
@@ -9,6 +14,7 @@ import javafx.scene.control.TextField;
 
 public class UniversityAddController {
 
+    private EventBus eventBus = EventBusSystem.getEventBus();
 
     @FXML
     private TextField textFieldNameOfUniverity;
@@ -23,8 +29,46 @@ public class UniversityAddController {
     private TextField textFieldShortForm;
 
     @FXML
+    private Button ButtonUniversityAdd;
+
+    private UniversityDTO university;
+
+    @FXML
     void handleAddButton(ActionEvent event) {
+        university = new UniversityDTO();
+        boolean validationSuccessful = writeToUniversityDTO();
+
+        if (validationSuccessful) {
+            eventBus.post(new SuccessfullyAddedUniversityEvent(university));
+            ((Button)event.getSource()).getScene().getWindow().hide();
+        }
+
+    }
+    private boolean writeToUniversityDTO(){
+
+        FormValidator universityValidator = new FormValidator();
+
         String name = textFieldNameOfUniverity.getText();
+        String location =textFieldLocation.getText();
+        String country = textFieldCountry.getText();
+        String shortForm =textFieldShortForm.getText();
+
+        if(universityValidator.textFieldNotEmpty(textFieldNameOfUniverity)){
+            university.setName(name);
+        }
+        if(universityValidator.textFieldNotEmpty(textFieldCountry)){
+            university.setCountry(country);
+
+        }
+        if(universityValidator.textFieldNotEmpty(textFieldLocation)){
+            university.setLocation(location);
+
+        }
+        university.setAbbreviation(shortForm);
+
+        return universityValidator.validationSuccessful();
+
+
     }
 
     @FXML
