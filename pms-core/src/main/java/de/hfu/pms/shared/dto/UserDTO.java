fb4498fc90124130fda3d20e3570fd86081b21c8
David Geigle <33023777+David-Geigle@users.noreply.github.com>
2019-05-21 19:49:50 +0200

    Added empty UserDTO constructor

--- pms-core/src/main/java/de/hfu/pms/shared/dto/UserDTO.java
+++ pms-core/src/main/java/de/hfu/pms/shared/dto/UserDTO.java
@@ -64,6 +64,8 @@ public class UserDTO {
         this.userRole = userRole;
     }
 
+    public UserDTO(){}
+
     public UserDTO(String username, String password, String forename, String lastname, String email, UserRole userRole) {
         this.username = username;
         this.password = password;
