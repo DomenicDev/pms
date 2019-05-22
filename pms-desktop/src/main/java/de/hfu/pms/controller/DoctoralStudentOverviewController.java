02c1a142f1ad7f0d017f703e536d3118eda35b56
Colovan <colovan@gmail.com>
2019-05-22 16:25:11 +0200

    Added the possibility to select multiple entries inside the Preview Table (for deletion). It is now possible to deselect a selected entry.

--- pms-desktop/src/main/java/de/hfu/pms/controller/DoctoralStudentOverviewController.java
+++ pms-desktop/src/main/java/de/hfu/pms/controller/DoctoralStudentOverviewController.java
@@ -7,13 +7,16 @@ import de.hfu.pms.pool.EntityPool;
 import de.hfu.pms.shared.dto.PreviewDoctoralStudentDTO;
 import de.hfu.pms.shared.enums.FacultyHFU;
 import de.hfu.pms.shared.enums.Gender;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.util.Callback;
+import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
 import java.util.Arrays;
@@ -26,6 +29,8 @@ public class DoctoralStudentOverviewController implements Initializable {
 
     //private Logger logger = Logger.getLogger(DoctoralStudentOverviewController.class);
     private EventBus eventBus = EventBusSystem.getEventBus();
+    private ObjectProperty<TableRow<PreviewDoctoralStudentDTO>> selectedTableRow = new SimpleObjectProperty<>();
+
 
     @FXML
     private TextField searchTextField;
@@ -59,26 +64,30 @@ public class DoctoralStudentOverviewController implements Initializable {
 
     @FXML
     public void handleOnActionDeleteButton(ActionEvent event) {
-        if(searchResultTableView.getSelectionModel().getSelectedItems().size() == 1){
+        if(searchResultTableView.getSelectionModel().getSelectedItem() != null){
             // todo
             // ConfirmDialog (yes/no)
-            // server request: delete(Collection<Int>) (Id's to be deleted)
+            // server request: delete(Collection<Int>) (Id's of the DB-Entries to be deleted)
         }
         else{
-            eventBus.post(new AlertNotificationEvent(1, "Bitte wählen Sie einen Eintrag aus, welcher gelöscht werden soll."));
+            eventBus.post(new AlertNotificationEvent(1, "Bitte wählen Sie den zu löschenden Eintrag aus."));
         }
     }
 
     @FXML
     public void handleOnActionEditButton(ActionEvent event) {
-         if(searchResultTableView.getSelectionModel().getSelectedItems().size() == 1){
-             // get full DoctoalStudent
-             // Switch to edit window
-         }
-         else{
-             eventBus.post(new AlertNotificationEvent(1, "Bitte wählen Sie einen Eintrag aus, welcher editiert werden soll."));
-         }
-
+        int selectedCount = searchResultTableView.getSelectionModel().getSelectedItems().size();
+        if(selectedCount == 1){
+            // todo
+            // get full DoctoalStudent
+            // Switch to edit window
+        }
+        else if(selectedCount > 1){
+            eventBus.post(new AlertNotificationEvent(1, "Es kann immer nur ein Eintrag editiert werden."));
+        }
+        else {
+            eventBus.post(new AlertNotificationEvent(1, "Bitte wählen Sie den zu editierenden Eintrag aus."));
+        }
     }
 
     @FXML
@@ -92,17 +101,36 @@ public class DoctoralStudentOverviewController implements Initializable {
         PreviewDoctoralStudentDTO student1 = new PreviewDoctoralStudentDTO(500L, "Jahnsen", "Jan", FacultyHFU.Medical_and_Life_Sciences, "jan.jahnsen@mail.com", "017322497814", Gender.Male);
         PreviewDoctoralStudentDTO student2 = new PreviewDoctoralStudentDTO(501L, "Fröhlich", "Alina", FacultyHFU.Medical_and_Life_Sciences, "ali.fr@mail.com", "015121639248", Gender.Female);
 
-        Collection<PreviewDoctoralStudentDTO> students = new HashSet<PreviewDoctoralStudentDTO>(Arrays.asList(student1, student2));
+        Collection<PreviewDoctoralStudentDTO> students = new HashSet<>(Arrays.asList(student1, student2));
 
         searchResultTableView.getItems().addAll(students);
     }
 
     private void initPreviewTables() {
+        searchResultTableView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
         searchResultNameTableColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
         searchResultForeNameTableColumn.setCellValueFactory(new PropertyValueFactory<>("foreName"));
         searchResultFacultyTableColumn.setCellValueFactory(new PropertyValueFactory<>("faculty"));
         searchResultEmailTableColumn.setCellValueFactory(new PropertyValueFactory<>("email"));
         searchResultPhoneNumberTableColumn.setCellValueFactory(new PropertyValueFactory<>("phoneNumber"));
         searchResultGenderTableColumn.setCellValueFactory(new PropertyValueFactory<>("gender"));
+
+        searchResultTableView.setRowFactory(new Callback<TableView<PreviewDoctoralStudentDTO>, TableRow<PreviewDoctoralStudentDTO>>() {
+            @Override
+            public TableRow<PreviewDoctoralStudentDTO> call(TableView<PreviewDoctoralStudentDTO> tableView2) {
+                final TableRow<PreviewDoctoralStudentDTO> row = new TableRow<>();
+                row.addEventFilter(MouseEvent.MOUSE_PRESSED, new EventHandler<>() {
+                    @Override
+                    public void handle(MouseEvent event) {
+                        final int index = row.getIndex();
+                        if (index >= 0 && index < searchResultTableView.getItems().size() && searchResultTableView.getSelectionModel().isSelected(index)) {
+                            searchResultTableView.getSelectionModel().clearSelection();
+                            event.consume();
+                        }
+                    }
+                });
+                return row;
+            }
+        });
     }
 }
