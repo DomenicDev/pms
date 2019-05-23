35a373dcdf1fbb0fbd8ee9fe1ec39f5ae2ea7b76
Domenic Cassisi <domenic.cassisi@web.de>
2019-05-23 10:13:42 +0200

    Extended test doctoral student for representation on the client

--- pms-webserver/src/main/java/de/hfu/pms/DatabaseInit.java
+++ pms-webserver/src/main/java/de/hfu/pms/DatabaseInit.java
@@ -5,11 +5,15 @@ import de.hfu.pms.dao.UniversityDao;
 import de.hfu.pms.dao.UserDao;
 import de.hfu.pms.model.*;
 import de.hfu.pms.shared.enums.FacultyHFU;
+import de.hfu.pms.shared.enums.FamilyStatus;
 import de.hfu.pms.shared.enums.Gender;
+import de.hfu.pms.shared.enums.Salutation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
 
 /**
  * This service adds some test (sample) entities to the database.
@@ -38,29 +42,40 @@ public class DatabaseInit implements CommandLineRunner {
         testAdmin.setRole(UserRole.ADMIN);
         userDao.save(testAdmin);
 
-        // add simple doctoral student
-        DoctoralStudent student = new DoctoralStudent();
-
-        PersonalData personalData = new PersonalData();
-        personalData.setForename("Max");
-        personalData.setLastName("Mustermann");
-        personalData.setGender(Gender.Male);
-        personalData.setNationality("German");
-        student.setPersonalData(personalData);
-
-        student.getTargetGraduation().setFacultyHFU(FacultyHFU.Informatik);
-        student.getQualifiedGraduation().setGrade("1.3");
-        personalData.getAddress().setCountry("Germany");
-        personalData.setEmail("test@email.de");
-
-
-        doctoralStudentDao.save(student);
-
         // add test university
         University university = new University();
         university.setName("HFU");
         university.setLocation("Furtwangen");
         university.setCountry("Deutschland");
-        universityDao.save(university);
+        university = universityDao.save(university);
+
+        // add simple doctoral student
+        DoctoralStudent student = new DoctoralStudent();
+
+        PersonalData personalData = new PersonalData();
+        personalData.setSalutation(Salutation.Mr);
+        personalData.setForename("Max");
+        personalData.setLastName("Mustermann");
+        personalData.setGender(Gender.Male);
+        personalData.setNationality("German");
+        personalData.setFamilyStatus(FamilyStatus.Single);
+        personalData.setNumberOfChildren(0);
+        personalData.setEmail("max@mustermann.de");
+        personalData.setDateOfBirth(LocalDate.of(1997, 12, 19));
+        personalData.getAddress().setStreet("Musterstraße 1");
+        personalData.getAddress().setLocation("Musterort");
+        personalData.getAddress().setPlz("12345");
+        personalData.getAddress().setCountry("Germany");
+        student.setPersonalData(personalData);
+
+        student.getTargetGraduation().setFacultyHFU(FacultyHFU.Informatik);
+        student.getQualifiedGraduation().setGrade("1.3");
+
+        student.getQualifiedGraduation().setGradedUniversity(university);
+
+
+
+
+        doctoralStudentDao.save(student);
     }
 }
