5abc20e5cc21908fc0f4cb0aab2d88af88b0a679
Benedikt Neubauer <benni.neubauer@gmx.net>
2019-06-20 14:09:46 +0200

    deleted hyperlinks on startscreen and added all faculty alerts and labels to the stings propertys bundle

--- pms-desktop/src/main/java/de/hfu/pms/controller/FacultyEditController.java
+++ pms-desktop/src/main/java/de/hfu/pms/controller/FacultyEditController.java
@@ -14,12 +14,13 @@ import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
 import java.net.URL;
-import java.util.*;
+import java.util.Optional;
+import java.util.ResourceBundle;
 
 public class FacultyEditController implements Initializable {
     private EventBus eventBus = EventBusSystem.getEventBus();
     private static Logger logger = Logger.getLogger(EntityPool.class);
-
+    private ResourceBundle bundle;
     private FacultyDTO faculty;
 
     @FXML
@@ -31,6 +32,7 @@ public class FacultyEditController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         eventBus.register(this);
+        this.bundle =resources;
 
         // init list view with available faculties
         updateListView();
@@ -39,9 +41,9 @@ public class FacultyEditController implements Initializable {
     @FXML
     void handleAddButton(ActionEvent event) {
         TextInputDialog dialog = new TextInputDialog();
-        dialog.setTitle("Fakultät hinzufügen");
-        dialog.setHeaderText("Sie sind dabei eine neue Fakultät anzulegen.");
-        dialog.setContentText("Name der neuen Fakultät:");
+        dialog.setTitle(bundle.getString("ui.label.header.addFaculty"));
+        dialog.setHeaderText(bundle.getString("ui.label.content.addFaculty"));
+        dialog.setContentText(bundle.getString("ui.label.name.addFaculty"));
         Optional<String> result = dialog.showAndWait();
 
         if (result.isPresent()){
@@ -51,21 +53,21 @@ public class FacultyEditController implements Initializable {
                 EventBusSystem.getEventBus().post(new RequestAddFacultyEvent(newFaculty));
                 return;
             }
-            eventBus.post(new AlertNotificationEvent(1, "Bitte geben sie einen gültigen Fakultätsnamen ein"));
+            eventBus.post(new AlertNotificationEvent(AlertNotificationEvent.INFO, bundle.getString("ui.alert.addFaculty")));
         }
     }
 
     @FXML
     void handleEditButton(ActionEvent event){
         if(facultiesListView.getSelectionModel().getSelectedItem() == null){
-            eventBus.post(new AlertNotificationEvent(1, "Bitte wählen Sie die zu editierende Fakultät aus."));
+            eventBus.post(new AlertNotificationEvent(AlertNotificationEvent.INFO, bundle.getString("ui.alert.editFaculty")));
             return;
         }
 
         TextInputDialog dialog = new TextInputDialog(facultiesListView.getSelectionModel().getSelectedItem().getFacultyName());
-        dialog.setTitle("Fakultät bearbeiten");
-        dialog.setHeaderText("Sie sind dabei eine Fakultät umzubenennen.");
-        dialog.setContentText("Neuer Name der Fakultät:");
+        dialog.setTitle(bundle.getString("ui.label.header.editFaculty"));
+        dialog.setHeaderText(bundle.getString("ui.label.content.editFaculty"));
+        dialog.setContentText(bundle.getString("ui.label.name.editFaculty"));
         Optional<String> result = dialog.showAndWait();
 
         if (result.isPresent()){
@@ -77,7 +79,7 @@ public class FacultyEditController implements Initializable {
                 EventBusSystem.getEventBus().post(new RequestUpdateFacultyEvent(faculty));
                 return;
             }
-            eventBus.post(new AlertNotificationEvent(1, "Bitte geben sie einen gültigen Fakultätsnamen ein"));
+            eventBus.post(new AlertNotificationEvent(AlertNotificationEvent.INFO,bundle.getString("ui.alert.addFaculty")));
         }
         //updateListView();
     }
@@ -87,14 +89,14 @@ public class FacultyEditController implements Initializable {
         FacultyDTO toBeDeleted = facultiesListView.getSelectionModel().getSelectedItem();
 
         if(facultiesListView.getSelectionModel().getSelectedItem() == null){
-            eventBus.post(new AlertNotificationEvent(1, "Bitte wählen Sie die zu löschende Fakultät aus."));
+            eventBus.post(new AlertNotificationEvent(AlertNotificationEvent.INFO,bundle.getString("ui.alert.deleteFaculty_selected")));
             return;
         }
 
         //confirm dialog
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-        alert.setTitle("Fakultät löschen");
-        alert.setHeaderText("Sie sind dabei folgende Fakultät zu löschen.");
+        alert.setTitle(bundle.getString("ui.alert.deleteFaculty"));
+        alert.setHeaderText(bundle.getString("ui.alert.delete_finally_faculty"));
         alert.setContentText(toBeDeleted.getFacultyName());
         Optional<ButtonType> result = alert.showAndWait();
         if (result.get() == ButtonType.OK){
